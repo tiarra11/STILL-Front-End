cb84e493946f0aaa8936d415d2f6aecd0a8e49ba
ProspersMartin <martinprospers1@gmail.com>
2019-06-28 15:42:02 -0400

    imported the dashboard scss file into the index.js file, which got deleted when we had the push-pull fiasco on wednesday, June 26th, 2019

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ import "bootstrap/dist/css/bootstrap.css";
 //include your index.scss file into the bundle
 import "../styles/index.scss";
 
+//include your index.scss file into the bundle
+import "../styles/dashboard.scss";
+
 //import your own components
 import Layout from "./layout";
 
