7310bd78960dd969ead9f45bd12c4086cc08aef3
ProspersMartin <martinprospers1@gmail.com>
2019-06-28 15:37:30 -0400

    added the dashboard scss file which got deleted when we had the push-pull fiasco on wednesday, June 26th, 2019

--- webpack.common.js
+++ webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
       Dropdown: "exports-loader?Dropdown!bootstrap/js/dist/dropdown"
     }),
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
+        // favicon: '4geeks.ico',
         template: 'template.html'
     })
   ]
