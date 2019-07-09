f8a87dd5d101f1aab84b223bbc120517dfa3e55f
ProspersMartin <martinprospers1@gmail.com>
2019-07-09 14:48:52 -0400

    Commented out all of the files in the ZunusedBoilerPlateViews folder

--- src/js/ZunusedBoilerplateViews/port_copy.js
+++ src/js/ZunusedBoilerplateViews/port_copy.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "../../styles/index.scss";
+// import React from "react";
+// import "../../styles/index.scss";
 
-import { CardStrategy } from "../component/cardStrategy";
-export class Portfolio extends React.Component {
-	render() {
-		return (
-			<div className="card-deck p-5">
-				<CardStrategy />
-			</div>
-		);
-	}
-}
+// import { CardStrategy } from "../component/cardStrategy";
+// export class Portfolio extends React.Component {
+// 	render() {
+// 		return (
+// 			<div className="card-deck p-5">
+// 				<CardStrategy />
+// 			</div>
+// 		);
+// 	}
+// }
